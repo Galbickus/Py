2f6ca5dda12312860e80b182773e65f86061a4b1
Veronica <vgalbicka@gmail.com>
2024-11-05 08:56:50 -0300

    Create suma_naturalees_for.py

---
+++ clase9/suma_naturalees_for.py
@@ -0,0 +1,13 @@
+numero = 6
+suma = 0
+
+for i in range(1,numero + 1):
+    suma+=i
+print(f"La suma de los números naturales desde 1 hasta el {numero} es: {suma}")
+
+
+#for i in range(6):
+ #   print(f"Número: {i}")
+
+
+ 
